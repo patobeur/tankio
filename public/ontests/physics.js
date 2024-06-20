36f9aa397073806566d6589e9914f043753b643b
Patobeur <patobeur41@gmail.com>
2024-06-21 00:33:09 +0200

    Collision ok

---
+++ public/ontests/physics.js
@@ -0,0 +1,169 @@
+const _physics = {
+	physicBodies: [],
+	physicBodiesIndexed: {},
+
+	getAnId: function (num) {
+		let id = ''
+		for (let i = 0; i < num; i++) {
+			id += String.fromCharCode(65 + Math.floor(Math.random() * 26))
+		}
+		return id
+	},
+	Rectangle: (
+		function () {
+			function Rectangle(htmlElement, pushit = true) {
+				let angle = 0, transform = false
+
+				this.htmlElement = htmlElement;
+				this.width = htmlElement.clientWidth;
+				this.height = htmlElement.clientHeight;
+
+				if (typeof htmlElement.style.transform === 'string' && htmlElement.style.transform != '') {
+					transform = parseFloat(htmlElement.style.transform.replace(/rotate\(|deg\)/g, ''));
+					angle = transform ?? 0;
+				}
+				this.angle = angle
+				this.setCorners(angle);
+
+				if (pushit === true) {
+					_physics.physicBodies.push(this)
+					htmlElement.dataId = _physics.getAnId(32)
+					_physics.physicBodiesIndexed[htmlElement.dataId] = this
+				}
+				console.log('Rectangle', (pushit ? 'physicBodies' : 'div'), this)
+				console.log('htmlElement.dataId', htmlElement.dataId)
+			}
+
+			function sin(x) {
+				return Math.sin(x / 180 * Math.PI);
+			}
+
+			function cos(x) {
+				return Math.cos(x / 180 * Math.PI);
+			}
+
+			function getVectorLength(x, y, width, height) {
+				var center = {
+					x: x + width / 2,
+					y: y + height / 2
+				};
+				//console.log('center: ',center);
+				var vector = {
+					x: (x - center.x),
+					y: (y - center.y)
+				};
+				return Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+			}
+
+			function getRotatedTopLeftCornerOfRect(x, y, width, height, angle) {
+				var center = {
+					x: x + width / 2,
+					y: y + height / 2
+				};
+				//console.log('center: ',center);
+				var vector = {
+					x: (x - center.x),
+					y: (y - center.y)
+				};
+				//console.log('vector: ',vector);
+				var rotationMatrix = [[cos(angle), -sin(angle)], [sin(angle), cos(angle)]];
+				//console.log('rotationMatrix: ',rotationMatrix);
+				var rotatedVector = {
+					x: vector.x * rotationMatrix[0][0] + vector.y * rotationMatrix[0][1],
+					y: vector.x * rotationMatrix[1][0] + vector.y * rotationMatrix[1][1]
+				};
+				//console.log('rotatedVector: ',rotatedVector);
+				return {
+					x: (center.x + rotatedVector.x),
+					y: (center.y + rotatedVector.y)
+				};
+			}
+
+			function getOffset(el) {
+				var _x = 0;
+				var _y = 0;
+				while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+					_x += el.offsetLeft - el.scrollLeft;
+					_y += el.offsetTop - el.scrollTop;
+					el = el.offsetParent;
+				}
+				return {
+					top: _y,
+					left: _x
+				};
+			}
+
+			function pointInPoly(verties, testx, testy) {
+				var i, j, c = 0;
+				let nvert = verties.length;
+				for (i = 0, j = nvert - 1; i < nvert; j = i++) {
+					if (((verties[i].y > testy) != (verties[j].y > testy)) && (testx < (verties[j].x - verties[i].x) * (testy - verties[i].y) / (verties[j].y - verties[i].y) + verties[i].x))
+						c = !c;
+				}
+				return c;
+			}
+
+			function testCollision(rectangle) {
+				var collision = false;
+				this.getCorners().forEach(function (corner) {
+					var isCollided = pointInPoly(rectangle.getCorners(), corner.x, corner.y);
+					if (isCollided) collision = true;
+				});
+				return collision;
+			}
+
+			function checkRectangleCollision(rect, rect2) {
+				if (testCollision.call(rect, rect2)) return true;
+				else if (testCollision.call(rect2, rect)) return true;
+				return false;
+			}
+
+			function getAngleForNextCorner(anc, vectorLength) {
+				var alpha = Math.acos(anc / vectorLength) * (180 / Math.PI);
+				return 180 - alpha * 2;
+			}
+
+			Rectangle.prototype.setCorners = function (angle) {
+				this.originalPos = getOffset(this.htmlElement);
+				this.leftTopCorner = getRotatedTopLeftCornerOfRect(this.originalPos.left, this.originalPos.top, this.width, this.height, angle);
+
+				var vecLength = getVectorLength(this.originalPos.left, this.originalPos.top, this.width, this.height);
+				//console.log('vecLength: ',vecLength);
+
+				angle = angle + getAngleForNextCorner(this.width / 2, vecLength);
+				//console.log('angle: ',angle);
+				this.rightTopCorner = getRotatedTopLeftCornerOfRect(this.originalPos.left, this.originalPos.top, this.width, this.height, angle);
+
+				angle = angle + getAngleForNextCorner(this.height / 2, vecLength);
+				//console.log('angle: ',angle);
+				this.rightBottomCorner = getRotatedTopLeftCornerOfRect(this.originalPos.left, this.originalPos.top, this.width, this.height, angle);
+
+				angle = angle + getAngleForNextCorner(this.width / 2, vecLength);
+				//console.log('angle: ',angle);
+				this.leftBottomCorner = getRotatedTopLeftCornerOfRect(this.originalPos.left, this.originalPos.top, this.width, this.height, angle);
+
+				// console.log(this);
+			};
+
+			Rectangle.prototype.getCorners = function () {
+				return [this.leftTopCorner, this.rightTopCorner, this.rightBottomCorner, this.leftBottomCorner];
+			};
+
+			Rectangle.prototype.isCollided = function (rectangle) {
+				return checkRectangleCollision(this, rectangle);
+			};
+			return Rectangle;
+		}
+	)(),
+
+	checkcollisionRect: function (A, B, checkcollisionCallback) {
+		A.setCorners(A.angle);
+		B.setCorners(B.angle);
+
+		checkcollisionCallback(B)
+
+		if (A.isCollided(B)) return true;
+		return false
+	}
+}
+export { _physics }
---
+++ public/ontests/testCollisions.js
@@ -0,0 +1,49 @@
+import { _physics } from './physics.js'
+import { _front } from '../js/game/board.js'
+
+var allDivs = {};
+
+var cont = _front.createDiv({ tag: 'div', attributes: { className: 'map', textContent: '' }, style: { position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'white' } })
+var msgDiv = _front.createDiv({ tag: 'div', attributes: { className: 'msg', textContent: 'ok' }, })
+
+allDivs['PLAYER'] = new _physics.Rectangle(_front.createDiv({ tag: 'div', attributes: { id: 'player', className: 'player', textContent: '' }, style: { position: 'absolute', borderRadius: '50%', backgroundColor: '#FF0000', width: '32px', height: '32px', transform: 'rotate(28.9deg)', top: '140px', left: '250px' } }), false);
+allDivs['wall_1'] = new _physics.Rectangle(_front.createDiv({ tag: 'div', attributes: { className: 'wall', textContent: 'W1' }, style: { position: 'absolute', backgroundColor: '#00FF00', width: '250px', height: '75px', transform: 'rotate(16deg)', top: '60px', left: '180px' } }));
+allDivs['wall_2'] = new _physics.Rectangle(_front.createDiv({ tag: 'div', attributes: { className: 'wall', textContent: 'W2' }, style: { position: 'absolute', backgroundColor: '#0000FF', width: '60px', height: '275px', transform: 'rotate(16deg)', top: '160px', left: '280px' } }));
+
+document.body.appendChild(cont)
+document.body.appendChild(msgDiv)
+cont.appendChild(allDivs['wall_1'].htmlElement)
+cont.appendChild(allDivs['wall_2'].htmlElement)
+cont.appendChild(allDivs['PLAYER'].htmlElement)
+
+window.requestAnimFrame = function () {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame;
+}();
+
+let checkcollisionCallback = (element) => {
+	element.htmlElement.style.transform = 'rotate(' + (element.angle += 5.5) + 'deg)';
+}
+function draw() {
+	let collisions = 0
+	allDivs['PLAYER'].htmlElement.style.transform = 'rotate(' + (allDivs['PLAYER'].angle += 1.2) + 'deg)';
+
+	_physics.physicBodies.forEach(element => {
+		if (_physics.checkcollisionRect(allDivs['PLAYER'], element, checkcollisionCallback)) collisions++
+	});
+
+	if (collisions > 0) {
+		msgDiv.textContent = collisions + ' collision' + (collisions > 1 ? 's' : '') + ' detected!';
+	}
+	else {
+		if (msgDiv.textContent != '') { msgDiv.textContent = '' }
+	}
+
+	setTimeout(function () {
+		window.requestAnimFrame(draw);
+	}, 50);
+}
+window.requestAnimFrame(draw);
